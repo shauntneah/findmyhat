662c1f777543dfc25c119ff30f0ed945e6a04528
Shaun <8938302+shauntneah@users.noreply.github.com>
2023-04-10 16:42:20 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,155 @@
+// package dependencies
+const prompt = require('prompt-sync')({ sigint: true });
+const clear = require('clear-screen');
+
+
+// pre-defined variables by test question
+const stone = 'x';
+const hole = 'o';
+const fieldCharacter = '░';
+const pathCharacter = '¥';
+const height = 10;
+const width = 10;
+
+// random chance of a hole in the field
+const percentHoles = 0.08; // 8% of the field is holes, per minimum requirement by Jean (8% * (10 * 10 game board))  
+
+
+// A function to tell a simple story background to set the mood for game
+function tellStory() {
+  console.log("\x1b[36m%s\x1b[0m", "A prisoner in a dungeon maze, by the name of Yen, learns of a magical teleportation stone that can help him escape.");
+  prompt('\x1b[90mPress Enter to continue.\x1b[0m');
+  console.log("\x1b[36m%s\x1b[0m", "He decides to search for the stone to aid his escape.");
+  prompt('\x1b[90mPress Enter to continue.\x1b[0m');
+  console.log("\x1b[36m%s\x1b[0m", "The maze is filled with dangerous sink-holes that make the journey difficult.");
+  prompt('\x1b[90mPress Enter to continue.\x1b[0m');
+  console.log("\x1b[36m%s\x1b[0m", "Despite the challenges, the prisoner remains determined to find the stone and escape.");
+  prompt('\x1b[90mPress Enter to continue.\x1b[0m');
+  console.log("\x1b[36m%s\x1b[0m", "Here the adventure begins... Help the prisoner find the teleportation stone and escape the maze!!");
+  prompt('\x1b[91mPress Enter to start the game.\x1b[0m');
+}
+
+tellStory();
+playGame();
+
+
+// A function to generate the field, with the given dimensions and percentage of holes
+function generateField(height, width, percentHoles) {
+  const field = new Array(height).fill(0).map(() => new Array(width));
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const prob = Math.random();
+      field[y][x] = prob > percentHoles ? fieldCharacter : hole; // see percentHoles. 8% of the field is holes
+    }
+  }
+  return field;
+}
+
+//generate the gameboard field
+function printField(field) {
+  clear(); //clears the screen, after story is told
+  field.forEach(row => console.log(row.join('')));
+}
+
+//setup the avatar movement
+function getNextPosition(direction, position) {
+  switch (direction) {
+    case 'up':
+      return [position[0] - 1, position[1]];
+    case 'right':
+      return [position[0], position[1] + 1];
+    case 'down':
+      return [position[0] + 1, position[1]];
+    case 'left':
+      return [position[0], position[1] - 1];
+    default:
+      return position;
+  }
+}
+
+//checks if the avatar is out of bounds
+function isOutOfBounds(position, height, width) {
+  return (
+    position[0] < 0 ||
+    position[0] >= height ||
+    position[1] < 0 ||
+    position[1] >= width
+  );
+}
+
+function playGame() {
+
+  const field = generateField(height, width, percentHoles); //generates the field 
+
+  let playerPosition = [0, 0]; //starting position of the avatar
+  let stonePosition = [Math.floor(Math.random() * height), Math.floor(Math.random() * width)];
+  while (stonePosition[0] === 0 && stonePosition[1] === 0) {
+    stonePosition = [Math.floor(Math.random() * height), Math.floor(Math.random() * width)];
+  }
+  field[playerPosition[0]][playerPosition[1]] = pathCharacter;
+  field[stonePosition[0]][stonePosition[1]] = stone;
+
+  // main game loop
+  let playing = true;
+  while (playing) {
+    printField(field);
+
+    //prompt for user input
+    const direction = prompt('Which direction? (Up / Down / Left / Right)').toLowerCase();
+    const nextPosition = getNextPosition(direction, playerPosition);
+
+    //checks if the avatar is out of bounds
+    if (isOutOfBounds(nextPosition, height, width)) {
+      console.log('Out of bounds! Game over. You\'ve got whacked by a baton weilding cop! Stay inside the sandbox! ');
+      playing = false;
+      break;
+    }
+    const nextTile = field[nextPosition[0]][nextPosition[1]];
+    //checks if the avatar is on a hole
+    if (nextTile === hole) {
+      console.log('Oh no... you fell into a sink-hole. Game over.');
+      playing = false;
+      break;
+
+      //checks if the avatar is on the teleportation stone
+    } else if (nextTile === stone) {
+      console.log('\x1b[36m%s', 'X marks the spot!\x1b[0m', 'Congratulations, you found the teleportation stone!', '\x1b[33mNow Grade me A please. :)\x1b[0m');
+      playing = false;
+      break;
+    }
+
+    //moves the avatar
+    field[nextPosition[0]][nextPosition[1]] = pathCharacter;
+    playerPosition = nextPosition;
+  }
+}
+
+//Avatar movement keys
+function getNextPosition(direction, position) {
+  switch (direction.toLowerCase()) {
+    case 'u': //to allow single letter input
+    case 'up': //this is full text input
+      return [position[0] - 1, position[1]];
+    case 'r': //to allow single letter input
+    case 'right':
+      return [position[0], position[1] + 1];
+    case 'd': //to allow single letter input
+    case 'down':
+      return [position[0] + 1, position[1]];
+    case 'l': //to allow single letter input
+    case 'left':
+      return [position[0], position[1] - 1];
+    default:
+      return position;
+  }
+}
+
+
+// marking x as red on the game board
+function printField(field) {
+  clear();
+  field.forEach(row => {
+    const coloredRow = row.map(char => char === stone ? `\x1b[31m${char}\x1b[0m` : char);
+    console.log(coloredRow.join(''));
+  });
+}
---
+++ readme.md
@@ -0,0 +1,109 @@
+
+
+#  Javascript Assessment Test - Requirement Document
+
+
+
+This was an test evaluation assignment as part of [GenerationSG](https://singapore.generation.org/launch-your-career-in-tech/) Junior Fullstack Developer Bootcamp, Day 26.
+
+## Technical Requirement:
+
+Apply Javascript to build a web application (Find Your Hat game) from scratch, using functions and node.js packages.
+
+### **Find your hat game application**
+
+![img](https://lh5.googleusercontent.com/EsOWRk-Z18OBeHBGEJooeiLyLMy0Y-tqqMezJwOVxNRNkWQsDiHw5Z8AAtojYli4JcGbqJrEfbpCXe4imto4Ng6I5HM2GSFmSpHlv-7eJZvoZlk_i22s-LIcTY8WhvQob8ImcV3HApc)
+
+### Requirements: 
+
+- Create a 10 X 10 Fields
+
+- The character * can be always at the default of position (0,0) when the user starts the game
+
+- The controls for the character are:
+
+- - Up = U or u key
+  - Down = D or d key
+  - Left = L or l key
+  - Right = R or r key
+
+- If a user entered an invalid key (e.g. p or t), output “Enter (u, d, l or r) and allow the user to enter again
+
+- The number of holes generated should be lesser than the fields generated
+
+- When the character drops into a hole, output “Sorry, you fell down a hole!” and the game ends
+
+- When the character hits the boundaries, output “Out of bounds - Game End!” and the game ends
+
+- When the character gets the hat, output “Congrats, you found your hat!” and and the game ends
+
+- <span style = 'color:gold'>**We were allow to improvish without affecting assestment criteria. As a result, I have slightly modified the prompt output messages to better align with the game's backdrop storyline ** </span>
+
+
+
+### **Starter Code**
+
+Create global variables and import prompt input from package (Please create your own global variables if required)
+
+```
+const prompt = require('prompt-sync')({sigint: true});
+const clear = require('clear-screen');
+
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+const row = 10;
+const col = 10;
+```
+
+
+
+----------
+
+## My project screenshots
+
+![](https://i.ibb.co/S5dfXHV/1.png)
+
+![](https://i.ibb.co/SwqTgpP/2.png)
+
+
+
+----------
+
+## **Reflect** on the following questions in your readme file
+
+Q1. What did you like about this project?
+Ans: While coding this game, it brought back memories of dial-up modem (from 9600 baud rate onwards. [soundbite](https://youtu.be/QDS4B0mM-ew)) when the 'Web' was more commonly referred to as Dial-up and not as the Internet. The 'Internet' term came later for where I was from (Singapore).
+
+![](https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8yNTYyODQwMi9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTY1MTk0MDY0OH0.gdAYFls9prmRxcnpaB_QU3Gs8Naj4i6iZ7Uc-1FxdEc/img.jpg?width=980)
+
+*source: [IEEE Spectrum](https://spectrum.ieee.org/social-medias-dialup-ancestor-the-bulletin-board-system)*
+
+The web was like a Wild Wild West, full of BBS (bulletin board systems) waiting to be discovered and explored. It was in this setting where I was first exposed to what I consider the first real online gaming, navigating around games that were beautifully crafted by ASCII-based art. As a young boy, I remember eagerly searching for these BBS with text-based RPG games, which I enjoyed immensely. Till this day, I recall my favourite local BBS was called Ken's Den, a free hobbyist BBS, available only for 4 hours per day. It had the best ascii-art, D&D styled Adventure RPG game and in which I was only allowed 60 movement action per day in game, at my then BBS membership level.
+
+With this project challenge, and given the limitation of available time per the assessment requirement, I've attempted to create a reminisce feel of such a BBS text-based RPG game, ableit a very, very simplified version.  
+
+
+
+----------
+
+
+
+Q2. What did you struggle with in this project?
+Ans: working with [Math.random() method](https://www.w3schools.com/js/js_random.asp), to incorporate random holes, was not something we were taught in class. Considerable reading, followed by trial and error was needed to get the desired outcome 
+
+
+
+----------
+
+
+
+Q3. What would make your experience with this assessment better?
+Ans: A little more time would be nice, if we were expected to pick up a new code command while having our first code attempt at Javascript.  
+
+
+
+
+
